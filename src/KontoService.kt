686110d7ba067604642b7c71893ad2df9ffe1f0a
Toren Gebauer <toren.gebauer@iteratec.at>
2022-05-09 11:19:53 +0200

    m

--- src/KontoService.kt
+++
@@ -1,8 +0,0 @@
-class KontoService {
-    companion object {
-        @JvmStatic
-        fun findMinKontoByInhaber(konten: MutableList<Konto>, i: Int): Konto? {
-            return konten.filter { it.kontoInhaberId == i }.minByOrNull { it.kontostand }
-        }
-    }
-}
